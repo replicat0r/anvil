f7fb6aac62d6373d92fc9b75133a9d299deca0d3
replicat0r <replicat0r.misc@gmail.com>
2018-04-25 17:21:16 +0000

    added slugs to link url with elixir protocal

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,6 +1,11 @@
 defmodule Anvil.Video do
   use Anvil.Web, :model
-
+  @primary_key {:id, Anvil.Permalink, autogenerate: true}
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
   schema "videos" do
     field :url, :string
     field :title, :string
@@ -20,9 +25,25 @@ defmodule Anvil.Video do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
+  
+  
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+  
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      IO.puts("asdasdsa")
+      changeset
+    end
+  end
+  
+  defp slugify(str) do
+    str |> String.downcase() |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
